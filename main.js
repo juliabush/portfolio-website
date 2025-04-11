8167a81ab87707185c330ace28d5d316e7edf9ab
juliabush <j.elizabethbush@gmail.com>
2025-04-11 21:44:34 +0200

    Creating the infite loop

--- main.js
+++ main.js
@@ -3,3 +3,18 @@ const track = document.getElementById('carousel-track');
 
 const clone = track.cloneNode(true);
 track.appendChild(clone);
+
+let scrollAmount = 0;
+let isPaused = false;
+
+function scrollCarousel() {
+    if (!isPaused) {
+        scrollAmount += 1;
+        familyCarousel.scrollLeft = scrollAmount;
+
+        if (scrollAmount >= track.scrollWidth/2) {
+            scrollAmount = 0;
+            familyCarousel.scrollLeft = 0;
+        }
+    }
+}
